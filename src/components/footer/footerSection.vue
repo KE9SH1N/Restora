97add234526aeef83629761a9aaa7d159405ace4
Md. Muttashim Mishel Mawn <k3nshi9@gmail.com>
2021-09-19 14:35:43 +0600

    adding footer components for this project

---
+++ src/components/footer/footerSection.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <!-- footer section  -->
+
+<section class="footer">
+
+    <div class="share">
+        <a href="#" class="btn">facebook</a>
+        <a href="#" class="btn">twitter</a>
+        <a href="#" class="btn">instagram</a>
+        <a href="#" class="btn">pinterest</a>
+        <a href="#" class="btn">linkedin</a>
+    </div>
+
+    <h1 class="credit"> created by <span> mr. web designer </span> | all rights reserved! </h1>
+
+</section>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    /* footer section starts here */
+.footer{
+  background:#000;
+  text-align: center;
+}
+
+.footer .share{
+  display: flex;
+  gap:1.5rem;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.footer .credit{
+  padding:2.5rem 1rem;
+  color:#fff;
+  font-weight: normal;
+  font-size: 2rem;
+}
+
+.footer .credit span{
+  color:var(--red);
+}
+
+/* footer section ends here */
+</style>
---
+++ src/components/footer/scrollTop.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <!-- scroll top button  -->
+        <a href="#home" class="fas fa-angle-up" id="scroll-top"></a>
+    </div>
+</template>
+<script>
+// export default {
+    
+// }
+
+window.onscroll = () =>{
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
+}
+</script>
+
+
+<style scoped>
+    /* scroll top section starts here */
+#scroll-top{
+  position: fixed;
+  top:-120%;
+  right:2rem;
+  padding:.5rem 1.5rem;
+  font-size: 4rem;
+  background:var(--red);
+  color:#fff;
+  border-radius: .5rem;
+  transition: 1s linear;
+  z-index: 1000;
+}
+
+#scroll-top.active{
+  top:calc(100% - 12rem)
+}
+/* scroll top section ends here */
+</style>
